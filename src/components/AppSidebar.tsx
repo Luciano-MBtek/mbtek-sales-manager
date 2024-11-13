7846a62fdff348747c70ff5c03798323e42ca1e1
Luciano E. Perez <info.luciando.design@gmail.com>
2024-11-13 14:41:04 -0300

    update appsidebar-contact

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -13,6 +13,7 @@ import {
 } from "@/components/ui/sidebar";
 import NavUser from "./SideBarFooter";
 import SideBarContactGroup from "./SideBarContactGroup";
+import Link from "next/link";
 
 // Menu items.
 const items = [
@@ -49,10 +50,10 @@ export function AppSidebar() {
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild>
-                    <a href={item.url}>
+                    <Link href={item.url}>
                       <item.icon />
                       <span>{item.title}</span>
-                    </a>
+                    </Link>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
--- src/components/SideBarContactGroup.tsx
+++ src/components/SideBarContactGroup.tsx
@@ -1,5 +1,12 @@
 "use client";
-import { ChevronRight, type LucideIcon } from "lucide-react";
+import {
+  ChevronRight,
+  CircleUser,
+  NotepadText,
+  Table,
+  UserRoundCheck,
+  Box,
+} from "lucide-react";
 import {
   Collapsible,
   CollapsibleContent,
@@ -18,24 +25,90 @@ import {
   SidebarMenuSubItem,
 } from "@/components/ui/sidebar";
 import { usePathname, useSearchParams } from "next/navigation";
+import Link from "next/link";
 
 const SideBarContactGroup = () => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
+  const showContactGroup = pathname?.match(/^\/contacts\/[^/]+/);
+  if (!showContactGroup) return null;
 
+  const contactPath = "/contacts/";
+  const id = pathname.slice(contactPath.length).split("/")[0];
   const name = searchParams.get("name");
   const lastname = searchParams.get("lastname");
 
-  const showContactGroup = pathname?.match(/^\/contacts\/[^/]+$/);
-  if (!showContactGroup) return null;
+  const propertiesPath = `/contacts/${id}/properties`;
+  const mainPath = `/contacts/${id}`;
+  const isPropertiesActive = pathname === propertiesPath;
+  const isMainActive = pathname === mainPath;
+
+  console.log(propertiesPath);
+  const items = [
+    {
+      title: "Lead Qualification",
+      url: "/forms/discovery-lead",
+      icon: UserRoundCheck,
+    },
+    {
+      title: "Single Product Quote",
+      url: "/forms/single-product",
+      icon: Box,
+    },
+  ];
+
   return (
     <SidebarGroup>
       <SidebarGroupLabel>Contact {`${name} ${lastname}`}</SidebarGroupLabel>
+
       <SidebarMenu>
-        <Collapsible>
+        <SidebarMenuItem>
+          {isMainActive ? (
+            <SidebarMenuButton isActive={true}>
+              <CircleUser />
+              <span>Main</span>
+            </SidebarMenuButton>
+          ) : (
+            <SidebarMenuButton asChild>
+              <Link
+                href={{
+                  pathname: mainPath,
+                  query: { name, lastname },
+                }}
+              >
+                <CircleUser />
+                <span>Main</span>
+              </Link>
+            </SidebarMenuButton>
+          )}
+        </SidebarMenuItem>
+        <SidebarMenuItem>
+          {isPropertiesActive ? (
+            <SidebarMenuButton isActive={true}>
+              <Table />
+              <span>Properties</span>
+            </SidebarMenuButton>
+          ) : (
+            <SidebarMenuButton asChild>
+              <Link
+                href={{
+                  pathname: propertiesPath,
+                  query: { name, lastname },
+                }}
+              >
+                <Table />
+                <span>Properties</span>
+              </Link>
+            </SidebarMenuButton>
+          )}
+        </SidebarMenuItem>
+        <Collapsible defaultOpen={true}>
           <CollapsibleTrigger asChild>
             <SidebarMenuItem>
-              <SidebarMenuButton>Forms</SidebarMenuButton>
+              <SidebarMenuButton>
+                <NotepadText />
+                Forms
+              </SidebarMenuButton>
               <SidebarMenuAction className="transition-transform duration-200 data-[state=open]:rotate-90">
                 <ChevronRight />
                 <span className="sr-only">Toggle Forms</span>
@@ -43,19 +116,20 @@ const SideBarContactGroup = () => {
             </SidebarMenuItem>
           </CollapsibleTrigger>
           <CollapsibleContent asChild>
-            <SidebarMenuSub>
-              <SidebarMenuSubItem>
-                <SidebarMenuSubButton>Contact Form</SidebarMenuSubButton>
-              </SidebarMenuSubItem>
-              <SidebarMenuSubItem>
-                <SidebarMenuSubButton>Feedback Form</SidebarMenuSubButton>
-              </SidebarMenuSubItem>
-            </SidebarMenuSub>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
           </CollapsibleContent>
         </Collapsible>
-        <SidebarMenuItem>
-          <SidebarMenuButton>Properties</SidebarMenuButton>
-        </SidebarMenuItem>
       </SidebarMenu>
     </SidebarGroup>
   );
