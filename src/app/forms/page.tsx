c13ffcd4b0930d286ac0a3a9656c851c6bdba9af
Luciano E. Perez <info.luciano.design@gmail.com>
2024-12-12 14:36:45 -0300

    eliminados archivos de forms

--- src/app/forms/page.tsx
+++
@@ -1,6 +0,0 @@
-import { collectDataRoutes } from "../../types";
-import { redirect } from "next/navigation";
-
-export default function FormsPage() {
-  redirect(collectDataRoutes.DISCOVERY_CALL);
-}
--- src/app/forms/step-one/page.tsx
+++
@@ -1,9 +0,0 @@
-import StepOneForm from "./stepOneForm";
-
-export default function StepOne() {
-  return (
-    <div>
-      <StepOneForm />
-    </div>
-  );
-}
