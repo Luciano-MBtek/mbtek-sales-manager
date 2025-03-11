9ba04b6d1ad70d5eefd30a61dd520449e5d43a86
Luciano E. Perez <info.luciano.design@gmail.com>
2025-03-10 23:59:28 -0300

    fix chatbot message

--- src/components/ChatBot/ChatMessage.tsx
+++ src/components/ChatBot/ChatMessage.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 import { CopyButton } from "../CopyButton";
 import { DownloadButton } from "../DownloadButton";
 import { ThinkingDots } from "./ThinkingDots";
@@ -10,32 +12,6 @@ interface ChatMessageProps {
 }
 
 export default function ChatMessage({ message, isUser }: ChatMessageProps) {
-  const imageRegex = /!\[.*?\]\((.*?)\)/g;
-  const imageMatches = Array.from(message.matchAll(imageRegex));
-  const imageUrls = imageMatches.map((match) => match[1]);
-
-  const textWithoutImages = message.replace(imageRegex, "");
-
-  const ChatImage = (props: { src: string; alt?: string }) => {
-    return (
-      <div className="relative inline-block my-2">
-        <Image
-          alt={props.alt || "Image"}
-          src={props.src}
-          width={300}
-          height={300}
-          className="max-w-[300px] max-h-[300px] object-contain rounded-lg"
-        />
-        <div className="absolute bottom-1 right-2 p-1 bg-opacity-75 rounded hover:bg-opacity-100 transition">
-          <CopyButton string={props.src} />
-        </div>
-        <div className="absolute bottom-1 right-14 p-1 bg-opacity-75 rounded hover:bg-opacity-100 transition">
-          <DownloadButton url={props.src} />
-        </div>
-      </div>
-    );
-  };
-
   if (message === "..." && !isUser) {
     return <ThinkingDots />;
   }
@@ -43,31 +19,68 @@ export default function ChatMessage({ message, isUser }: ChatMessageProps) {
   return (
     <div className={`flex ${isUser ? "justify-end" : "justify-start"}`}>
       <div className="relative group">
+        {/* Button to copy full message (optional in the Assistant role) */}
         {!isUser && (
           <div className="absolute bottom-1 right-[11%] p-1 bg-opacity-75 rounded hover:bg-opacity-100 transition opacity-0 group-hover:opacity-100">
             <CopyButton string={message} />
           </div>
         )}
+
         <div
           className={`max-w-[90%] rounded-lg p-4 flex items-center flex-col justify-center ${
             isUser ? "bg-blue-500 text-white" : "bg-gray-100 text-gray-800"
           }`}
         >
+          {/* If it is the user, we display the text directly */}
           {isUser ? (
-            <p>{message}</p>
+            <span className="block">{message}</span>
           ) : (
-            <>
-              {/* Render markdown text without images */}
-              <ReactMarkdown className="prose dark:prose-invert">
-                {textWithoutImages}
-              </ReactMarkdown>
-              {/* Render images separately */}
-              <div className="mt-4">
-                {imageUrls.map((url, index) => (
-                  <ChatImage key={index} src={url} />
-                ))}
-              </div>
-            </>
+            <ReactMarkdown
+              className="prose dark:prose-invert"
+              remarkPlugins={[remarkGfm]}
+              components={{
+                p: ({ children }) => <span className="block">{children}</span>,
+                img({ node, ...props }) {
+                  if (props.src && props.src.includes("quickchart.io")) {
+                    return (
+                      <span className="relative inline-block my-2">
+                        <img
+                          alt={props.alt || "Image"}
+                          src={props.src}
+                          className="max-w-[300px] max-h-[300px] object-contain rounded-lg"
+                        />
+                        <span className="absolute bottom-1 right-2 p-1 bg-opacity-75 rounded hover:bg-opacity-100 transition">
+                          <CopyButton string={props.src} />
+                        </span>
+                        <span className="absolute bottom-1 right-14 p-1 bg-opacity-75 rounded hover:bg-opacity-100 transition">
+                          <DownloadButton url={props.src} />
+                        </span>
+                      </span>
+                    );
+                  }
+
+                  return (
+                    <span className="relative inline-block my-2">
+                      <Image
+                        alt={props.alt || "Image"}
+                        src={props.src || ""}
+                        width={300}
+                        height={300}
+                        className="max-w-[300px] max-h-[300px] object-contain rounded-lg"
+                      />
+                      <span className="absolute bottom-1 right-2 p-1 bg-opacity-75 rounded hover:bg-opacity-100 transition">
+                        <CopyButton string={props.src || ""} />
+                      </span>
+                      <span className="absolute bottom-1 right-14 p-1 bg-opacity-75 rounded hover:bg-opacity-100 transition">
+                        <DownloadButton url={props.src || ""} />
+                      </span>
+                    </span>
+                  );
+                },
+              }}
+            >
+              {message}
+            </ReactMarkdown>
           )}
         </div>
       </div>
