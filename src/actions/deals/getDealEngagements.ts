ce91ee0e62834d22d15095dbee3e2d86a28139df
Luciano-MBtek <automation@mbtek.com>
2025-06-23 20:00:50 -0300

    feat(deals): add action to fetch deal engagements

---
+++ src/actions/deals/getDealEngagements.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import { sleep } from "@/lib/utils";
+import { Engagement } from "@/types/engagementsTypes";
+
+export async function getDealEngagements(dealId: string) {
+  const baseURL = `https://api.hubapi.com/engagements/v1/engagements/associated/deal/${dealId}`;
+  let allResults: Engagement[] = [];
+  let hasMore = true;
+  let offset = 0;
+
+  try {
+    while (hasMore) {
+      const URL = `${baseURL}${offset ? `?offset=${offset}` : ""}`;
+      const response = await fetch(URL, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${process.env.HUBSPOT_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        next: { tags: [`engagements`], revalidate: 300 },
+      });
+
+      if (response.ok) {
+        const jsonData = await response.json();
+        allResults = [...allResults, ...jsonData.results];
+        hasMore = jsonData.hasMore;
+        offset = jsonData.offset;
+        if (hasMore) await sleep(100);
+      } else {
+        const data = await response.json();
+        return { error: "Failed to retrieve data", details: data };
+      }
+    }
+    return { results: allResults };
+  } catch (error) {
+    console.error("Error fetching engagements:", error);
+    return null;
+  }
+}
